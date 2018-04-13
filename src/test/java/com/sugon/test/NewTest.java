f3d0288be47154cda0870e6ed6e3400463588c17
DesiNF <a312567415@qq.com>
2018-04-13 17:27:29 +0800

    3333

---
+++ src/test/java/com/sugon/test/NewTest.java
@@ -0,0 +1,54 @@
+package com.sugon.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+import com.sugon.Shell;
+import com.sugon.Util;
+
+public class NewTest {
+	@Test(priority = 0)
+	public void install() {
+		Shell shell = new Shell(Util.ip, Util.username, Util.password);
+		shell.execute("sh " + Util.install + " " + Util.password + " " + Util.ip);
+		// shell.execute("sh /root/untar.sh 123456 172.16.0.93");
+		ArrayList<String> stdout = shell.getStandardOutput();
+		for (String str : stdout) {
+			System.out.println(str);
+		}
+	}
+
+	@Test(priority = 1)
+	public void createDataSet() throws Exception {
+		String s;
+		Process proc = Runtime.getRuntime().exec("python AUS/createdataset.py");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(proc.getInputStream(), "gbk"));
+		while ((s = bufferedReader.readLine()) != null)
+			System.out.println(s);
+		proc.waitFor();
+	}
+
+	@Test(priority = 2)
+	public void insertData() {
+		Shell shell = new Shell(Util.ip, Util.username, Util.password);
+		shell.execute("sh " + Util.insertdataset);
+		// shell.execute("sh /root/insertdataset.sh");
+		ArrayList<String> stdout = shell.getStandardOutput();
+		for (String str : stdout) {
+			System.out.println(str);
+		}
+	}
+	@Test(priority = 3)
+	public void testMQL() throws Exception {
+		String s;
+		Process proc = Runtime.getRuntime().exec("python AUS/testMQL.py");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(proc.getInputStream(), "gbk"));
+		while ((s = bufferedReader.readLine()) != null)
+			System.out.println(s);
+		proc.waitFor();
+	}
+}
